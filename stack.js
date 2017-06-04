dbe453b9ea29b901b8d9a8f5889301406a526868
3dw1nm0535 <workockmoses@gmail.com>
2017-06-04 21:38:59 +0300

    stack constructor

---
+++ stack.js
@@ -0,0 +1,36 @@
+//Stack() implementation using constructor method
+function Stack() {
+  this.dataStore = [];
+  this.top = 0;
+  this.push = push;
+  this.clear = clear;
+  this.length = length;
+  this.pop = pop;
+  this.peek = peek;
+}
+
+//push() element to the top of the stack and increment the top variable
+function push(element) {
+  this.dataStore[this.top++] = element;
+}
+
+//pop() return the element at the top of the stack and decrement the top variable
+function pop() {
+  return this.dataStore[--this.top]
+}
+
+//peek() peek at the top element in the stack
+function peek() {
+  return this.dataStore[this.top-1];
+}
+
+//length() returns the length of the stack element
+function length() { 
+  return this.top;
+}
+
+//clear() clears the stack elements
+function clear () {
+  this.top = 0;
+  this.dataStore = [];
+}
